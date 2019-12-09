8fe0505a7e232807db42d2be5187f495733b8381
kentataniguchi <tkmshtsbnymn_d@ezweb.ne.jp>
2019-12-09 15:09:14 +0900

    モデル追加

---
+++ KonbiniBrazor/Shared/BaseItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonbiniBrazor.Shared
+{
+    public class BaseItem
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 商品リンク
+        /// </summary>
+        public string Namelink { get; set; }
+
+        /// <summary>
+        /// 値段
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// 地域
+        /// </summary>
+        public string Area { get; set; }
+    }
+}
