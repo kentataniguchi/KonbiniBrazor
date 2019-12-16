08a1e421570386e7d7569a3e7b5c10f2bc479b02
kentataniguchi <tkmshtsbnymn_d@ezweb.ne.jp>
2019-12-16 14:15:37 +0900

    各種モデルクラス作成

---
+++ KonbiniBrazor/Shared/Sweets.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonbiniBrazor.Shared
+{
+    public class Sweets:BaseItem
+    {
+    }
+}
